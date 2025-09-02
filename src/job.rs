652414f0be8d1c67047914bd634d26092175e485
Thynkon <thynkon@protonmail.com>
2025-09-02 10:49:56 +0200

    fix(tests): fix job test

--- src/job.rs
+++ src/job.rs
@@ -360,7 +360,8 @@ mod tests {
         "agent_id": "550e8400-e29b-41d4-a716-446655440002",
         "action": {
             "cmd": "echo",
-            "args": ["hi"]
+            "args": ["hi"],
+            "variant": "my_variant"
         }
     }
     "#;
